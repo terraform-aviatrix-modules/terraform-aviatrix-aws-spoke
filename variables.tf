3ac3bc3958da486b20a42f34bdf0cab024437e03
Dennizz <root@ipaddr.nl>
2020-09-05 15:32:10 +0200

    formatting

--- variables.tf
+++ variables.tf
@@ -1,56 +1,56 @@
 variable "name" {
-  description = "Name for this spoke VPC and it's gateways"  
-  type = string
+  description = "Name for this spoke VPC and it's gateways"
+  type        = string
 }
 
 variable "region" {
-  description = "The AWS region to deploy this module in"  
-  type = string
+  description = "The AWS region to deploy this module in"
+  type        = string
 }
 
 variable "cidr" {
-  description = "The CIDR range to be used for the VPC"  
-  type = string
+  description = "The CIDR range to be used for the VPC"
+  type        = string
 }
 
 variable "account" {
-  description = "The AWS account name, as known by the Aviatrix controller"  
-  type = string
+  description = "The AWS account name, as known by the Aviatrix controller"
+  type        = string
 }
 
 variable "instance_size" {
-  description = "AWS Instance size for the Aviatrix gateways"  
-  type    = string
-  default = "t3.medium"
+  description = "AWS Instance size for the Aviatrix gateways"
+  type        = string
+  default     = "t3.medium"
 }
 
 variable "ha_gw" {
   description = "Boolean to determine if module will be deployed in HA or single mode"
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "insane_mode" {
   description = "Set to true to enable Aviatrix high performance encryption."
-  type    = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "az1" {
-  description = "Concatenates with region to form az names. e.g. eu-central-1a. Only used for insane mode"  
-  type    = string
-  default = "a"
+  description = "Concatenates with region to form az names. e.g. eu-central-1a. Only used for insane mode"
+  type        = string
+  default     = "a"
 }
 
 variable "az2" {
-  description = "Concatenates with region to form az names. e.g. eu-central-1b. Only used for insane mode"  
-  type    = string
-  default = "b"
+  description = "Concatenates with region to form az names. e.g. eu-central-1b. Only used for insane mode"
+  type        = string
+  default     = "b"
 }
 
 variable "transit_gw" {
-  description = "Name of the transit gateway to attach this spoke to"  
-  type = string
+  description = "Name of the transit gateway to attach this spoke to"
+  type        = string
 }
 
 variable "active_mesh" {
