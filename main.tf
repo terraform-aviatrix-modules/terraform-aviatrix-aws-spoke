de8ab5a674193dd7b91b1ffab7d62924ced9621f
Dennizz <root@ipaddr.nl>
2020-10-19 23:20:10 +0200

    make sure spoke_gw resource does not interfere with attachement status

--- main.tf
+++ main.tf
@@ -10,19 +10,20 @@ resource "aviatrix_vpc" "default" {
 }
 #Spoke GW
 resource "aviatrix_spoke_gateway" "default" {
-  enable_active_mesh = var.active_mesh
-  cloud_type         = 1
-  vpc_reg            = var.region
-  gw_name            = local.name
-  gw_size            = var.instance_size
-  vpc_id             = aviatrix_vpc.default.vpc_id
-  account_name       = var.account
-  subnet             = local.subnet
-  ha_subnet          = var.ha_gw ? local.ha_subnet : null
-  ha_gw_size         = var.ha_gw ? var.instance_size : null
-  insane_mode        = var.insane_mode
-  insane_mode_az     = local.insane_mode_az
-  ha_insane_mode_az  = var.ha_gw ? local.ha_insane_mode_az : null
+  enable_active_mesh                = var.active_mesh
+  cloud_type                        = 1
+  vpc_reg                           = var.region
+  gw_name                           = local.name
+  gw_size                           = var.instance_size
+  vpc_id                            = aviatrix_vpc.default.vpc_id
+  account_name                      = var.account
+  subnet                            = local.subnet
+  ha_subnet                         = var.ha_gw ? local.ha_subnet : null
+  ha_gw_size                        = var.ha_gw ? var.instance_size : null
+  insane_mode                       = var.insane_mode
+  insane_mode_az                    = local.insane_mode_az
+  ha_insane_mode_az                 = var.ha_gw ? local.ha_insane_mode_az : null
+  manage_transit_gateway_attachment = false
 }
 
 resource "aviatrix_spoke_transit_attachment" "default" {
