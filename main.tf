eba8d04cc307214ada0eee4ea7b55a40b78b65a9
Dennizz <root@ipaddr.nl>
2020-10-19 23:10:13 +0200

    lets see if this logic is allowed

--- main.tf
+++ main.tf
@@ -23,19 +23,18 @@ resource "aviatrix_spoke_gateway" "default" {
   insane_mode        = var.insane_mode
   insane_mode_az     = local.insane_mode_az
   ha_insane_mode_az  = var.ha_gw ? local.ha_insane_mode_az : null
-  #manage_transit_gateway_attachment = false
-  #transit_gw         = var.transit_gw
-}
-
-resource "aviatrix_segmentation_security_domain_association" "default" {
-  count                = var.attached ? (length(var.security_domain) > 0 ? 1 : 0) : 0 #Only create resource when attached and security_domain is set.
-  transit_gateway_name = var.transit_gw
-  security_domain_name = var.security_domain
-  attachment_name      = aviatrix_spoke_gateway.default.gw_name
 }
 
 resource "aviatrix_spoke_transit_attachment" "default" {
   count           = var.attached ? 1 : 0
   spoke_gw_name   = aviatrix_spoke_gateway.default.gw_name
   transit_gw_name = var.transit_gw
+}
+
+resource "aviatrix_segmentation_security_domain_association" "default" {
+  count                = var.attached + length(var.security_domain) > 0 ? 1 : 0 #Only create resource when attached and security_domain is set.
+  transit_gateway_name = var.transit_gw
+  security_domain_name = var.security_domain
+  attachment_name      = aviatrix_spoke_gateway.default.gw_name
+  depends_on           = [aviatrix_spoke_transit_attachment.default] #Let's make sure this cannot create a race condition
 }
